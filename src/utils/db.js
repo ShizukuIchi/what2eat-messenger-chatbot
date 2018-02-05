069854d20884d4a56027086cd6677119198add3b
shizuku <eddy540511@yahoo.com.tw>
2018-02-06 02:18:51 +0800

    db promise reject

--- src/utils/db.js
+++ src/utils/db.js
@@ -9,24 +9,33 @@ class DB {
     this.client.connect();
   }
   getDatasTable() {
-    return new Promise((res) => {
+    return new Promise((res, rej) => {
       this.client.query('SELECT * FROM datas')
         .then(result => res(result.rows))
-        .catch(e => {throw e})
+        .catch(e => {
+          rej(e);
+          throw e
+        })
     })
   }
   getData(name) {
-    return new Promise((res) => {
+    return new Promise((res, rej) => {
       this.client.query(`SELECT data->'elements' FROM datas WHERE data->'name' = '"${name}"'`)
         .then(result => res(result.rows[0]['?column?']))
-        .catch(e => {throw e})
+        .catch(e => {
+          rej(e)
+          throw e
+        })
     })
   }
   isDataExist(name) {
-    return new Promise(res => {
+    return new Promise((res, rej) => {
       this.client.query(`SELECT EXISTS(SELECT 1 FROM datas WHERE data->'name' = '"${name}"' )`)
         .then(result => {console.log(result);res(result.rows[0].exists)})
-        .catch(e => {throw e;})
+        .catch(e => {
+          rej(e)
+          throw e;
+        })
     }) 
   }
   async insertData(name, elements = []) {
@@ -42,23 +51,26 @@ class DB {
       }
       this.client.query(query)
         .then(result => res(result.rows))
-        .catch(e => {throw e})
+        .catch(e => {
+          rej(e)
+          throw e
+        })
     })
   }
   async insertDataElements(name, elements) {
     const exist = await this.isDataExist(name)
-    if(!exist) {
-      console.log(`no ${name}.`)
-      return
-    }
-    const query = `
-      UPDATE datas
-      SET data = jsonb_set(
-        data::jsonb,
-        '{elements}',
-        (data->'elements')::jsonb || '${JSON.stringify(elements)}'::jsonb) 
-      WHERE data->'name' = '"${name}"'`
     return new Promise((res, rej) => {
+      if(!exist) {
+        console.log(`no ${name}.`)
+        rej(`no ${name}`)
+      }
+      const query = `
+        UPDATE datas
+        SET data = jsonb_set(
+          data::jsonb,
+          '{elements}',
+          (data->'elements')::jsonb || '${JSON.stringify(elements)}'::jsonb) 
+        WHERE data->'name' = '"${name}"'`
       this.client.query(query)
         .then(result => res(result))
         .catch(e => {
@@ -69,22 +81,25 @@ class DB {
   }
   async deleteDataElement(name, element) {
     const exist = await this.isDataExist(name)
-    if(!exist) {
-      console.log(`no ${name}.`)
-      return
-    }
-    const query = `
-      UPDATE datas
-      SET data = jsonb_set(
-        data::jsonb,
-        '{elements}',
-        (data->'elements')::jsonb - '${element}'::jsonb) 
-      WHERE data->'name' = '"${name}"';`
+    return new Promise((res, rej) => {
+      if(!exist) {
+        console.log(`no ${name}.`)
+        rej(`no ${name}`)
+      }
+      const query = `
+        UPDATE datas
+        SET data = jsonb_set(
+          data::jsonb,
+          '{elements}',
+          (data->'elements')::jsonb - '${element}'::jsonb) 
+        WHERE data->'name' = '"${name}"';`
     
-    return new Promise(res => {
       this.client.query(query)
         .then(result => res(result))
-        .catch(e => {throw e})
+        .catch(e => {
+          rej(e)
+          throw e
+        })
     })
   }
 
